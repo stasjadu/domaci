aa91cbed5eac66c976f29ad3006f02a4ff5c23c7
stasjadu <75794319+stasjadu@users.noreply.github.com>
2020-12-15 15:14:28 +0100

    Update Fifo.c

--- fifo/Fifo.c
+++ fifo/Fifo.c
@@ -9,7 +9,6 @@
 #include <linux/errno.h>
 #include <linux/device.h>
 #include <linux/wait.h>
-#include <linux/slab.h>
 
 
 MODULE_LICENSE("Dual BSD/GPL");
@@ -23,12 +22,12 @@ DECLARE_WAIT_QUEUE_HEAD(readQueue);
 DECLARE_WAIT_QUEUE_HEAD(writeQueue);
 
 #define FIFO_MAJOR 255
-#define BUFF_SIZE 16
+#define BUFF_SIZE 90
 
 int fifo[16];
 int pos=0;
 int endRead = 0;
-int n=1;
+
 
 
 
@@ -62,7 +61,6 @@ int fifo_close(struct inode *pinode, struct file *pfile)
 ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
 {
 		int ret;
-		int i,j;
 		char buff[BUFF_SIZE];
 		long int len = 0;
 		if (endRead){
@@ -76,19 +74,14 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 		if(pos > 0)
 		{
 			pos --;
-			for(j=0; j<n; j++){
-				len = scnprintf(buff+strlen(buff), BUFF_SIZE, "%#04x ", fifo[0]);
-				
-				for(i=0; i<15; i++) {
-					fifo[i]=fifo[i+1];
-				}
-				fifo[15]= -1;
-			}
-			ret = copy_to_user(buffer, buff, len*n);
-			if(ret)
-				return -EFAULT;
-				printk(KERN_INFO "Succesfully read\n");
-				endRead = 1;
+			len = scnprintf(buff, BUFF_SIZE, "%x ", fifo[pos]);
+			pos+=pos;
+			if(pos==16)
+				pos=0;
+				ret=copy_to_user(buffer, buff, len);
+				if(ret)
+					return -EFAULT;
+					printk(KERN_INFO "Succesfully read\n");
 		}
 		else
 		{
@@ -97,61 +90,55 @@ ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 		
 		wake_up_interruptible(&writeQ);
 		endRead=1;
-		return len*n;
+		return len;
 }
 		
-
 ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
 {
-		char *buff_in=(char *)kmalloc(length, GFP_KERNEL);
-		char *buff_inCopy;
-		char cut[5];
-		int value, ret;
+		char buff[BUFF_SIZE];
+		char *position;
+		char *n;
+		int value, ret,;
+		int state=3;
+	
 		ret = copy_from_user(buff, buffer, length);
 		if(ret)
 			return -EFAULT;
 			buff[length-1] = '\0';
-			buff_inCopy=buff_in;
 			
-			
-			strncpy(cut, buff_inCopy, 4); 
-			cut[4]='\0';
-			if ((buff_inCopy[4] != '\0') && (buff_inCopy[4] != ';') && (strncmp(cut, "num=", 4)!=0)){
-				printk(KERN_NOTICE "Expected format: 0x??;0x??;0x??..(16) of max value 0xFF (1)");
-				return length;
-
-			}else ifstrncmp(cut, "num=", 4)==0){                                                              // U slucaju num=komande
-    				strsep(&buff_inCopy, "=");                                                                             // odbaciti num=
-    				kstrtoint(buff_inCopy, 10, &n);                                                                    // broj iza = proglasiti za batch size
-    				printk(KERN_INFO "Batch size set to %d", n);
-    				return length;
-
-			while(1){
-    				strncpy(cut, buff_inCopy, 4);
-				cut[4] = '\0';
-				if (kstrtoint(cut, 0, &value) != 0){
-					  printk(KERN_NOTICE "%s received. Expected format: 0x??;0x??;0x??..(16) of max value 0xFF (2)", cut);
-					  return length;
-				} else if (value>FIFO_MAJOR || value<0){
-					printk(KERN_ERR "Max value is 0xFF");
-					return length;
-
-				while(pos>16)
+			if(wait_event_interruptible(writeQ, (pos < 16)))
+				return -ERESTARTSYS;
+			n=buff;
+				
+			while(state){
+    				position=strchr(n, ';');
+				if(position==NULL)
+					state=0;
+				else
+					*position='\0';
+				if(pos < 16)
 				{
-					printk(KERN_WARNING "Fifo full\n");
-					if(wait_event_interruptible(writeQ,(pos<16)))
-						return -ERESTARTSYS;
-
-					if(pos<16)
+					ret = sscanf(n,"%x",&value);
+					if(ret==1)
 					{
+						n=position+1;
+						printk(KERN_INFO "Succesfully wrote value %x", value);
 						fifo[pos]=value;
 						pos++;
-						printk(KERN_INFO "Succesfully wrote value %d", value);
-					
+						if(pos==16)
+							pos=0;
 					}
-					if (buff_inCopy[4] == '\0') break;
-					if (strsep(&buff_inCopy, ";") == NULL) break;
-			}
+					else
+					{
+						printk(KERN_WARNING "Wrong command format\n");
+					}
+			      }
+			      else
+			      {
+				     printk(KERN_WARNING "Fifo is full\n");  
+			      }
+			}		
+					
 			wake_up_interruptible(&readQ);
 			return length;
 }
